9c10ceb8bbdd4ff47db68dd1f256c0310cd52763
jagrati7305 <jagrati07.work@gmail.com>
2026-01-11 16:07:09 +0530

    Splash Screen Timer Added

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,8 +1,27 @@
+import 'dart:async';
+
 import 'package:create_random_shape/constants/app_colors.dart';
+import 'package:create_random_shape/screens/main_screen.dart';
 import 'package:flutter/material.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState(){
+    super.initState();
+
+    Timer(Duration(seconds: 2), (){
+      Navigator.pushReplacement(context, 
+      MaterialPageRoute(builder: (context)=>MainScreen()));
+    }
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,7 +37,7 @@ class SplashScreen extends StatelessWidget {
               Text(
                 "Let's Create",
                 style: TextStyle(
-                  color: Colors.white,
+                  color: AppColors.baseColor,
                   fontSize: 32,
                   fontWeight: FontWeight.bold
                 ),
@@ -33,6 +52,45 @@ class SplashScreen extends StatelessWidget {
               )
             ],
           ),
+        ),
+      ),
+      bottomNavigationBar: Container(
+        height: 50,
+        color: AppColors.secondaryColor,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Made with',
+                  style: TextStyle(
+                    color: AppColors.baseColor,
+                    fontSize: 14,
+                    fontWeight: FontWeight.w500
+                  ),
+                ),
+                SizedBox(
+                  width: 4,
+                ),
+                Icon(
+                  Icons.favorite,
+                  color: AppColors.baseColor,
+                  size: 14,
+                )
+              ],
+            ),
+            Text(
+              'Jagrati',
+              style: TextStyle(
+                    color: AppColors.baseColor,
+                    fontSize: 14,
+                    fontWeight: FontWeight.w700,
+                    letterSpacing: 0.75
+                  ),
+            )
+          ],
         ),
       ),
     );
