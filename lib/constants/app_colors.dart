d80693311ca3a0c03b9067141d9cb8465378d821
jagrati7305 <jagrati07.work@gmail.com>
2026-01-11 10:36:28 +0530

    App Color Pallet

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/animation.dart';
+
+class AppColors {
+  static final Color baseColor = Color(0xFFFDFBD4);
+  static final Color mainBoxColor = Color(0xFFC05800);
+  static final Color secondaryColor = Color(0xFF713600);
+  static final Color darkColor = Color(0xFF38240D);
+}
